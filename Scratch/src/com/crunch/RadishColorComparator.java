61894d79ab6aca4f6d029ae81a4b763a355296d3
Christopher Baker <christopher.baker@tlgcohort.com>
2023-02-28 12:12:22 -0800

    Commit

---
+++ Scratch/src/com/crunch/RadishColorComparator.java
@@ -0,0 +1,10 @@
+package com.crunch;
+
+import java.util.Comparator;
+
+class RadishColorComparator implements Comparator <Radish> {
+    @Override
+    public int compare(Radish r1, Radish r2) {
+        return r1.getColor().compareTo(r2.getColor());
+    }
+}
