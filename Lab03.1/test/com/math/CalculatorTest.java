107afb2b7a6daf4e680e54f871eb24a8ba42deb7
Christopher Baker <christopher.baker@tlgcohort.com>
2023-03-01 13:24:33 -0800

    Added lifecycle methods to Calc

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -17,22 +17,23 @@ public class CalculatorTest {
     private Calculator calc;
 
     @BeforeClass
-    public static void initializeEntireTestRun(){
+    public static void initializeEntireTestRun() {
         System.out.println("initializeEntireTestRun");
     }
+
     @AfterClass
-    public static void finalizeTestRun(){
+    public static void finalizeTestRun() {
         System.out.println("finalizeTestRun");
     }
 
     @Before
-    public void setUp(){
+    public void setUp() {
         System.out.println("setUp");
         calc = new Calculator();
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         System.out.println("tearDown");
         calc = new Calculator();
     }
@@ -41,20 +42,21 @@ public class CalculatorTest {
     public void testAdd() {
         System.out.println("testAdd");
         Calculator calc = new Calculator();
-      // int result = calc.add(1,4);
-      // assertEquals(5, result);
+        // int result = calc.add(1,4);
+        // assertEquals(5, result);   Last two lines can be written as.....>
         assertEquals(5, calc.add(1, 4));  // expected, actual
     }
 
     @Test
-    public void testDivide(){
+    public void testDivide() {
         System.out.println("testDivide");
         Calculator calc = new Calculator();
         assertEquals(2.5, calc.divide(5, 2), .001); //see notes for delta
 
     }
+
     @Test
-    public void testIsEven(){
+    public void testIsEven() {
         System.out.println("testIsEven");
         Calculator calc = new Calculator();
         assertTrue(calc.isEven(10));
