3aea9c363e236dcadcd1e078fb01697703607fcb
Christopher Baker <christopher.baker@tlgcohort.com>
2023-03-02 14:49:53 -0800

    TVTest Complete w/class input

--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,9 +12,33 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
+/*
+ *  "T" is really going to be something like MusicItem
+ *  ShoppingCart<MusicItem> cart = .... // what's the T? ... MusicItem
+ *  If they said ShoppingCart<MediaPlayer> cart = .... // what's the T? ... MediaPlayer
+ */
+
+public class ShoppingCart<T extends Product> {
   // storage for the cart's contents
-  private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+  private final Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+  public double total(){
+    double result = 0.0;
+
+    for(T item : items){
+      result += item.getPrice();
+    }
+
+    return result;
+  }
+
+  public Collection<T> allItems(){
+    return Collections.unmodifiableCollection(items);
+  }
+
+  public int size(){
+    return items.size();
+  }
   
   public void addItem(T item) {
     items.add(item);
--- Lab04.2/test/com/javatunes/product/OrderTest.java
+++ Lab04.2/test/com/javatunes/product/OrderTest.java
@@ -19,27 +19,35 @@ public class OrderTest {
   
   @Before
   public void init() {
-    productCart = new ShoppingCart<Product>();
+    productCart = new ShoppingCart<Product>();            //the T is Product
     productCart.addItem(new MusicItem("CD-501"));
     productCart.addItem(new MediaPlayer("MP3-LP150"));
     
-    musicCart = new ShoppingCart<MusicItem>();
+    musicCart = new ShoppingCart<MusicItem>();            //the T is MusicItem
     musicCart.addItem(new MusicItem("CD-521"));
     musicCart.addItem(new MusicItem("CD-514"));
     
-    mediaCart = new ShoppingCart<MediaPlayer>();
+    mediaCart = new ShoppingCart<MediaPlayer>();          //the T is MediaPlayer
     mediaCart.addItem(new MediaPlayer("AAC-PL233"));
   }
-  
+
+  @Test
+  public void testProcessCart() {
+    Order order1 = new Order("order-1");
+    order1.processCart(productCart);      //passing ShoppingCart<Product>
+
+    Order order2 = new Order("order-2");  //passing ShoppingCar<MusicItem>
+    order2.processCart(musicCart);
+
+    Order order3 = new Order("order-3");  //passing ShoppingCart<MediaItem>
+    order3.processCart(mediaCart);
+  }
+
   /**
    * TODO: create new Order for each cart above and pass cart to processOrder().
    *  For example, create three Order objects: "order-1", "order-2", "order-3."
    *  order-1 is for the contents of productCart,
    *  order-2 is for the contents of musicCart,
-   *  order-3 is for the contents of meduaCart.
+   *  order-3 is for the contents of mediaCart.
    */
-  @Test
-  public void testProcessCart() {
-    
-  }
 }
